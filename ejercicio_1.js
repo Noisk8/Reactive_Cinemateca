9619a622aa9dc8ebbd391a98b11ff023ac821345
Noisk8 <juansk8jaramillo@gmail.com>
2023-06-09 16:40:17 -0500

    Create ejercicio_1.js

---
+++ ejercicio_1.js
@@ -0,0 +1,23 @@
+  s0.initVideo("https://raw.githubusercontent.com/Noisk8/Reactive_Cinemateca/main/videos/prueba_1.mp4");
+src(s0) 
+  //.colorama(() => a.fft[2] * 0.1) 
+  //.scrollX(() => a.fft[2] * 0.1,0,1 )
+  //.scrollY(() => a.fft[2] * 0.5, 0,2)
+.thresh(() => a.fft[1] * 0.5,0.04) 
+  
+  .out()
+
+
+
+
+24 FPS 
+
+HOrizontal 1920 x 1080 
+
+vertica
+
+
+1080 x 1920 
+
+
+1080 x 180 
